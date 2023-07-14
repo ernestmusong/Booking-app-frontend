959d70fa38f9cf68d5ed31b66caf523ca12b77ce
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-07-14 14:58:20 +0530

    Sign up Session

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Home from 'components/Home';
 import Footer from 'components/Footer';
 import NavigationPanel from 'components/NavigationPanel';
 import MyReservations from 'components/MyReservations';
+import Login from 'components/session/Login';
+import SignUp from 'components/session/SignUp';
 
 const App = () => (
   <>
@@ -10,6 +12,8 @@ const App = () => (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="reservations" element={<MyReservations />} />
+      <Route path="login" element={<Login />} />
+      <Route path="signup" element={<SignUp />} />
     </Routes>
     <Footer />
   </>
--- src/components/session/Login.jsx
+++ src/components/session/Login.jsx
@@ -1,31 +1,51 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { login } from 'redux/session/sessionSlice';
 
 const Login = () => {
   const { loading } = useSelector((store) => store.session);
   const dispatch = useDispatch();
-  const [name, setName] = useState();
+  const [name, setName] = useState('');
   const [error, setError] = useState(null);
+
   const handleLogin = async (event) => {
     event.preventDefault();
     try {
       await dispatch(login({ name }));
       setName('');
+      setError(null);
     } catch (error) {
-      setError('Wrong Name please check');
+      setError('Wrong name, please check.');
     }
   };
+
   return (
-    <form onSubmit={handleLogin}>
-      {error && <p>{error}</p>}
-      <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
-      {loading ? (
-        <button type="submit" className="button log" disabled>login ....</button>
-      ) : (
-        <button type="submit" className="button log">log in</button>
-      )}
-    </form>
+    <div className="form-wrap">
+      <h3>Log in</h3>
+      <form onSubmit={handleLogin}>
+        <label htmlFor="name" className="form-label">
+          User Name:
+          <input
+            type="text"
+            className="form-control"
+            id="name"
+            placeholder="Your Username"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            aria-describedby="name-error"
+          />
+        </label>
+        {error && <p className="error-message" id="name-error">{error}</p>}
+
+        <div className="link mt-2 d-flex justify-content-between w-100">
+          <button type="submit" className="btn btn-primary" disabled={loading}>
+            {loading ? 'Logging in...' : 'Log In'}
+          </button>
+          <Link to="/" className="btn text-light btn-secondary">Sign up</Link>
+        </div>
+      </form>
+    </div>
   );
 };
 
