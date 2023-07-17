06e8c90c3be39f79356e84a97cbd7a023b2b4073
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-07-17 19:31:35 +0530

    Eslint Fix

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import '../styles/Button.css';
+
 function Button({ text, className, fun }) {
   return (
     <button type="button" className={className} onClick={fun}>{text}</button>
